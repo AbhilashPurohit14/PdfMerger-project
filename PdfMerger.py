131141dddee0361d29f071d7be6660d543a478f5
abhilashpurohit <123354654+AbhilashPurohit14@users.noreply.github.com>
2023-08-20 12:00:47 +0530

    Create PdfMerger.py

---
+++ PdfMerger.py
@@ -0,0 +1,11 @@
+import PyPDF2 as pd
+
+pdffiles= ["1.pdf","2.pdf"]
+merger = pd.PdfMerger()
+
+for filename in pdffiles:
+        pdfFile = open(filename, 'rb')
+        pdfReader = pd.PdfReader(pdfFile)
+        merger.append(pdfReader)
+pdfFile.close()
+merger.write('merged.pdf')
